f82f8913ebcf1722b34aa8b31048d01c7e6e6dff
Yaroslav Shul <slavikkardinal@gmail.com>
2023-05-18 20:05:08 +0300

    add new changes

---
+++ app.js
@@ -0,0 +1,54 @@
+// console.log("Shul");
+
+// let one = 1;
+// let lett = "b";
+
+// console.log(one + lett);
+
+// let one = 1;
+// let lett = one;
+// console.log(lett);
+
+
+
+// let user = {
+//     name: "Mr",
+//     age: 5,
+//     adult: false,
+//     newInfo: undefined,
+//     zero: null,
+// }
+
+// console.log(user);
+
+
+
+// const isAdult = confirm("Are you an adult?");
+
+// if (isAdult) {
+//     console.log("isAdult");
+// }
+
+
+
+// var firstName = "Yaroslav";
+// var lastName = "Shul";
+// var group = "team_ns";
+// var yearOfBirth = 2003;
+
+// var study = true;
+
+// var variable = null;
+// var myVariable = undefined;
+
+// console.log(typeof yearOfBirth, typeof study, typeof firstName);
+// console.log(typeof variable, typeof myVariable);
+
+
+
+
+// var log = prompt("login");
+// var mail = prompt("mail");
+// var pass = prompt("password");
+
+// confirm("Your login: " + log + " Your mail: " + mail + " Your password: " + pass);
